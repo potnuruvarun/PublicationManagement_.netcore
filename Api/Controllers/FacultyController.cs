ed0cb3d31735b8e1c1adcaa3ee75e731784659ec
POTNURU VARUN <101381435+potnuruvarun@users.noreply.github.com>
2023-08-03 09:45:08 +0530

    Add files via upload

---
+++ Api/Controllers/FacultyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PublicationManagement.Model.Publishmodel;
+using PublicationManagement.Services.Facultypublishing;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacultyController : ControllerBase
+    {
+        IFacultypublishingServices Services;
+        public FacultyController(IFacultypublishingServices _services)
+        {
+            Services = _services;  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Facultypublish(FacultyPublishingModel model)
+        {
+            Services.Publish(model);
+            return Ok();
+        }
+
+    }
+}
---
+++ Api/Controllers/PublishController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PublicationManagement.Model.Publishmodel;
+using PublicationManagement.Services.Facultypublishing;
+using PublicationManagement.Services.PublishServices;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishController : ControllerBase
+    {
+        IPublishServices services;
+        IFacultypublishingServices facultypublishing;
+
+        public PublishController(IPublishServices _services,IFacultypublishingServices _facultypublishing)
+        {
+            services = _services;
+            facultypublishing = _facultypublishing;
+
+        }
+        
+        [HttpGet]
+        [Route("Get")]
+        public async  Task <IActionResult> Get()
+        {
+            return Ok(await services.Alldata());
+        }
+
+
+        [HttpGet]
+        [Route("Studentdata")]
+        public async Task<IActionResult> Studentdata()
+        {
+            return Ok(await services.viewdata());
+        }
+
+        [HttpGet]
+        [Route("Facultydata")]
+
+        public async Task<IActionResult> Facultydata()
+        {
+            return Ok(await facultypublishing.viewdata());
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Studentpublish(PublishingModels model)
+        {
+            services.Publish(model);
+            return Ok();
+        }
+
+
+
+    }
+}
